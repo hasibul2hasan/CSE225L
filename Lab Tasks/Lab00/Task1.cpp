5b7cfe80f1da8532ac23f21c89e2d62b027ca1f5
Hasibul Hasan <hasanshantoemail@gmail.com>
2023-03-17 00:45:04 +0600

    push

--- Lab Tasks/Lab00/Task1.cpp
+++ Lab Tasks/Lab00/Task1.cpp
@@ -4,15 +4,13 @@ using namespace std;
 int main()
 {
 	int num1, num2, sum;
-    cout << "\n Sum of two numbers :\n";
+	cout << "\n Sum of two numbers :\n";
 	cout << "-------------------------\n";
 	cout << " Input 1st number : ";
-	cin >> num1 ;
+	cin >> num1;
 	cout << " Input 2nd number : ";
 	cin >> num2;
 	sum = num1 + num2;
-	cout <<" The sum of the numbers is : " << sum << endl;
-	cout << endl;
+	cout << sum << endl;
 	return 0;
 }
-
