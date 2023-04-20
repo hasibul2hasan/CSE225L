ef66675225491ec228a01693fd19d20ed262f18d
Hasibul Hasan <hasanshantoemail@gmail.com>
2023-04-20 19:09:22 +0600

    Push

--- Assignment/main.cpp
+++ Assignment/main.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *tail = dummy;
 
-cout << "Test of list: ";
-cout << "Test of list: ";
-cout << "Test of list: ";
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
 
+        if (l1) tail->next = l1;
+        if (l2) tail->next = l2;
 
+        ListNode *head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
+
+int main() {
+    // Example usage
+    ListNode *l1 = new ListNode(1);
+    l1->next = new ListNode(3);
+    l1->next->next = new ListNode(5);
+    
+
+    ListNode *l2 = new ListNode(2);
+    l2->next = new ListNode(4);
+    l2->next->next = new ListNode(6);
+    
+
+    Solution solution;
+    ListNode *merged = solution.mergeTwoLists(l1, l2);
+
+    while (merged) {
+        cout << merged->val << " ";
+        merged = merged->next;
+    }
+    cout << endl;
 
     return 0;
 }
